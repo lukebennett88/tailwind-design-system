cc26b90b6a1f8dcd9c1872e0d6c3d9e699cc7d37
Luke Bennett <hello@lukebennett.com.au>
2023-08-03 20:40:08 +1000

    WIP

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-zinc-900">
+    <html className="bg-zinc-900" lang="en">
       <body>{children}</body>
     </html>
   );
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,69 @@
+// @ts-check
+
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'next',
+		'turbo',
+		'prettier',
+	],
+	overrides: [
+		{
+			extends: [
+				'plugin:@typescript-eslint/strict',
+				'plugin:@typescript-eslint/stylistic',
+			],
+			files: ['**/*.ts'],
+			parser: '@typescript-eslint/parser',
+			rules: {
+				'@typescript-eslint/ban-ts-comment': OFF,
+				'@typescript-eslint/consistent-type-imports': [
+					WARN,
+					{ prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+				],
+				'@typescript-eslint/consistent-type-definitions': [WARN, 'type'],
+				'@typescript-eslint/no-empty-function': OFF,
+				'@typescript-eslint/no-explicit-any': OFF,
+				'@typescript-eslint/no-unused-vars': [
+					WARN,
+					{ argsIgnorePattern: '^_' },
+				],
+				'@typescript-eslint/no-var-requires': OFF,
+				'@typescript-eslint/no-non-null-assertion': OFF,
+				'@typescript-eslint/padding-line-between-statements': [
+					ERROR,
+					{ blankLine: 'always', next: '*', prev: 'block-like' },
+				],
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
   },
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@next/next/no-html-link-for-pages': OFF,
+		'@next/next/no-img-element': OFF,
+		curly: [ERROR, 'all'],
+		'jsx-a11y/no-redundant-roles': OFF,
+		'no-case-declarations': OFF,
+		'no-constant-condition': OFF,
+		'no-inner-declarations': OFF,
+		'no-mixed-spaces-and-tabs': OFF,
+		'no-undef': OFF,
+		'no-unused-expressions': OFF,
+		'padding-line-between-statements': OFF,
+		'prefer-const': WARN,
+		'react/jsx-sort-props': ERROR,
+		'react/no-unescaped-entities': OFF,
+	},
 };
